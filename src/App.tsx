cfb52a2285c53bfef3c14550c939a09b4d8a990c
Jayvardhan Bhaskar <jayvardhanbhaskar123@gmail.com>
2026-09-07 19:13:21 +0530

    Fixed deep links integration and redirect

--- src/App.tsx
+++ src/App.tsx
@@ -630,7 +630,9 @@ function Router() {
   );
 }
 function App() {
-  return <QueryClientProvider client={queryClient}><TooltipProvider><WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, '')}><Router /></WouterRouter><Toaster /></TooltipProvider></QueryClientProvider>;
+  return <QueryClientProvider client={queryClient}><TooltipProvider><WouterRouter>
+  <Router />
+</WouterRouter><Toaster /></TooltipProvider></QueryClientProvider>;
 }
 
 export default App;
